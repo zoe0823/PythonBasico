31106e3f9e6ee7de5c95baca6b534909d54108d6
zoe0823 <zoe1104skz@gmail.com>
2026-02-10 17:29:32 -0800

    Datos numericos de phyton

--- sesion_01.py
+++ sesion_01.py
@@ -22,3 +22,47 @@ print(float(10%3))
 print(float(25%4))
 print(float(16%2))
 print(float(10 / 3))
+
+# Variables
+print("==========variables=============")
+x = 100
+y = 1
+print(x + y)
+
+ventas = 1999991
+print("nuestras ventas fueron:", ventas)
+
+is_active = True
+print(is_active)
+
+game_over = False
+print(game_over)
+
+some_string = "Hola soy un string"
+print(some_string)
+
+print("=========Condicinales=========")
+edad = 15
+
+if (edad >= 18):
+    print("Si puedes entrar al bar!!!")
+else:
+    print("No puedes entrar a el bar!!!")
+
+mi_numero = int(input("¿Cual es el numero que deseas verificar?: "))
+print(f"El numero que deseas verificar es {mi_numero}")
+if mi_numero % 2 == 0:
+    print(f"El numero {mi_numero} es par!")
+else:
+    print(f"El numero {mi_numero} es impar!!")
+
+def par_impar(numero):
+    if numero % 2 == 0:
+        print(f"El numero {numero} es par!")
+    else: 
+        print(f"El numero {numero} es impar!!")
+
+print("==========Funcion par_impar==========")
+mi_numero = int(input("¿Cual es el numero que deseas verificar?: "))
+print(f"El numero que deseas verificar es {mi_numero}")
+print(par_impar(mi_numero))
