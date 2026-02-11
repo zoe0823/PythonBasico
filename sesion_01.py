0e81553459398c01b6c50e8fdb3ce3570f7c9c13
zoe0823 <zoe1104skz@gmail.com>
2026-02-10 16:18:47 -0800

    Datos numericos de phyton

---
+++ sesion_01.py
@@ -0,0 +1,24 @@
+# numeros
+print(int(7))
+print(float(7.7))
+print(type(7))
+print(type(7.7))
+print(int(1+2))
+print(int(10*2))
+print(int(1 + 4 - 2))
+print(float(1 + 2.0))
+
+# Operadores Matematicos
+# +
+# - 
+# *
+# /
+# **
+# % Modulo
+
+print(int(2**3))
+print(int(4**8))
+print(float(10%3))
+print(float(25%4))
+print(float(16%2))
+print(float(10 / 3))
