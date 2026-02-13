516d23bfc7323550e7dfafc8dbbc72287f924fce
zoe0823 <zoe1104skz@gmail.com>
2026-02-12 17:37:56 -0800

    Tercera sesion de python

--- sesion_03.py
+++ sesion_03.py
@@ -19,3 +19,40 @@ mi_lista_2 = ["lunes", "martes", "miercoles", "jueves", "viernes"]
 for i in mi_lista_2:
     if i !="lunes":
        print (f"Feliz {i}!")
+
+# While loop
+i = 0
+
+while i < 5:
+    i += 1
+    if i == 3:
+        continue
+    print(i)
+    if i == 4:
+        break
+
+else:
+    print("i es ahora mayor o igual a 5")
+
+#Practica 2
+#Dada la lista mi_lista_2 = ["lunes", "martes", "miercoles", "jueves", "viernes"]
+#Imprimir cada elemento de la lista 3 veces y cuando sea lunes no lo incluya
+# Pista: usa los dos tipos loops while y for en el mismo programa para logarlo
+# Resultado:
+# Martes
+# Miercoles 
+# Jueves
+# Viernes
+# Martes
+# Miercoles 
+# Jueves
+# Viernes
+# Martes
+# Miercoles 
+# Jueves
+# Viernes
+j = 0
+while j <4:
+    j += 1
+    if j == 4:
+        continue
