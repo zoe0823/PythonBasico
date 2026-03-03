bd70051386c89a752ed191cf8b66cbdef4225a56
zoe0823 <zoe1104@gmail.com>
2026-03-02 17:04:23 -0800

    empezar batalla

--- Zombie.py
+++ Zombie.py
@@ -1,20 +1,19 @@
-from Enemigo import * 
-import random 
+from Enemigo import *
+import random
 
 class Zombie(Enemigo):
     def __init__(self, puntos_energia=10, ataque=1):
-        super().__init__(tipo_enemigo = 'Zombie', puntos_energia = puntos_energia, ataque = ataque)
+        super().__init__(tipo_enemigo='Zombie', puntos_energia=puntos_energia, ataque=ataque)
 
     def habla(self):
-        print("*Hummmmm...*")
-
+        print("*Hummmm.....*")
 
     def propagar_enfermedad(self):
-        print("El Zombie esta tratando de propagar la enfermedad!!")
+        print("El Xombie esta tratando de propagar la enfermedad!!")
 
     def ataque_especial(self):
         print("Zombie ataque especial")
         funciona_ataque_especial = random.random() < 0.50
         if funciona_ataque_especial:
-            self.puntos_energia += 2
-            print("Zombie ha regenerado su energia con 2HP!")
+            self.ataque += 2
+            print('Zombie ha regenerado su energia con 2HP!!!!')
